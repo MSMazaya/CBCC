3141f6a2edb8c43432779ded8e32fa8394544e21
MazayaCode <msulthanmazaya@gmail.com>
2021-02-17 15:53:30 +0700

    Add question mockup

--- cbcc/src/components/Faq.js
+++ cbcc/src/components/Faq.js
@@ -4,6 +4,18 @@ const Faq = () => {
     return (
         <div className="faq">
             <h1 className="sub-title">FAQ</h1>
+            <div className="question">
+                <h1>Berapa hasil 1 + 1</h1>
+            </div>
+            <div className="question">
+                <h1>Berapa hasil 1 + 1</h1>
+            </div>
+            <div className="question">
+                <h1>Berapa hasil 1 + 1</h1>
+            </div>
+            <div className="question">
+                <h1>Berapa hasil 1 + 1</h1>
+            </div>
         </div>
     )
 }
