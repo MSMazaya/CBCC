0fae10512fdf3e5e0cf148bb45860f03515cf6b2
MazayaCode <msulthanmazaya@gmail.com>
2021-02-20 14:16:50 +0700

    Add timeline and sponsor

--- cbcc/src/components/mainpage/Sponsor.js
+++ cbcc/src/components/mainpage/Sponsor.js
@@ -4,6 +4,11 @@ const Sponsor = () => {
     return (
         <div className="sponsor">
             <h1 className="sub-title">Our Sponsors</h1>
+            <div className="logo-sponsor">
+                <img className="sejutacita-sponsor" src="sejutacita.png" alt="sejutacita"/>
+                <img className='sfl-sponsor' src="sfl.png" alt="sejutacita"/>
+            </div>
+        
         </div>
     )
 }
--- cbcc/src/components/mainpage/Timeline.js
+++ cbcc/src/components/mainpage/Timeline.js
@@ -4,6 +4,20 @@ const Timeline = () => {
     return (
         <div className="timeline">
             <h1 className="sub-title">Timeline</h1>
+            <div className="picker flex-row">
+                <a>Something</a>
+                <a>Something</a>
+            </div>
+            <div className="gap"></div>
+            <div className='flex-col'>
+                <div className="line"></div>
+                <div className="timeline-container flex-row">   
+                    <div className="circle"></div>
+                    <div className="circle"></div>
+                    <div className="circle"></div>
+                    <div className="circle"></div>
+                </div>
+            </div>
         </div>
     )
 }
