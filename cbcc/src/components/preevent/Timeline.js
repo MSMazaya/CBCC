3bbe7ddd5fb5507b5c20e4c823b88591ca4196df
MazayaCode <msulthanmazaya@gmail.com>
2021-02-21 15:44:13 +0700

    Update Timeline.js

--- cbcc/src/components/preevent/Timeline.js
+++ cbcc/src/components/preevent/Timeline.js
@@ -5,6 +5,10 @@ const Timeline = () => {
         <div className="preevent-timeline flex-col">
             <h1 className="sub-title">Timeline</h1>
             <div className='flex-row pointer-wrapper'>
+                <div className="pointer"></div>
+                <div className="pointer"></div>
+                <div className="pointer"></div>
+                <div className="pointer"></div>
             </div>
         </div>
     )
