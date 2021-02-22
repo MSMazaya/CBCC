c99eb7531e1b464226f0f7d5345977522d078a9a
Riandy Hasan <16520117@mahasiswa.itb.ac.id>
2021-02-22 22:45:59 +0800

    nguli temlen :(

--- cbcc/src/components/mainpage/Timeline.js
+++ cbcc/src/components/mainpage/Timeline.js
@@ -28,16 +28,16 @@ const Timeline = () => {
                 </div>
                 <div className="circle tl">
                 <div className="text">
-                    <a className="tgl2">20 Februari</a>
-                    <a className="live2">Live IG</a>
-                    <a className="title2">“Break Your Limit ”</a>
+                    <a className="tgl">20 Februari</a>
+                    <a className="live">Live IG</a>
+                    <a className="title">“Break Your Limit ”</a>
                 </div>
                 </div>
                 <div className="circle tl">
                 <div className="text">
-                    <a className="tgl3">20 Maret</a>
-                    <a className="live3">Live IG</a>
-                    <a className="title3">“Outstanding Students 101”</a>
+                    <a className="tgl2">20 Maret</a>
+                    <a className="live2">Live IG</a>
+                    <a className="title2">“Outstanding Students 101”</a>
                 </div>
                 </div>
             </motion.div>
@@ -45,13 +45,54 @@ const Timeline = () => {
             <motion.div
             exit={{y:10,opacity:0}}
             className="timeline-container flex-row">
-                <div className="circle tl"></div>
-                <div className="circle tl"></div>
-                <div className="circle tl"></div>
-                <div className="circle tl"></div>
-                <div className="circle tl"></div>
-                <div className="circle tl"></div>
-                <div className="circle tl"></div>
+                <div className="circle tl">
+                <div className="text">
+                <a className="tgl3">1 Maret</a>
+                <a className="title3">Open Registration </a>
+                </div>
+                </div>
+                <div className="circle tl">
+                <div className="text">
+                <a className="tgl4">27 Maret</a>
+                <a className="title4">Workshop</a>
+                </div>
+                </div>                
+                <div className="circle tl">
+                <div className="text">
+                <a className="tgl3">1 April </a>
+                <a className="title3">Case Contributor</a>
+                </div>
+                </div>                
+                <div className="circle tl">
+                <div className="text">
+                <a className="tgl5">7 April </a>
+                <a className="title5">Close Regisration</a>
+                </div>
+                </div>                
+                <div className="circle tl">
+                <div className="text">
+                <a className="tgl3">6-13 April</a>
+                <a className="title7">Individual Preliminary Challenge</a>
+                </div>
+                </div>               
+                <div className="circle tl">
+                <div className="text">
+                <a className="tgl6">24 April</a>
+                <a className="title6"> “How to Make a Proper Pitch Deck”</a>
+                </div>
+                </div>
+                <div className="circle tl">
+                <div className="text">
+                <a className="tgl7">1 Mei</a>
+                <a className="title7">Semifinal pitch deck presentation</a>
+                </div>
+                </div>
+                <div className="circle tl">
+                <div className="text">
+                <a className="tgl6">2 Mei</a>
+                <a className="title6">Final pitch deck presentation & Awarding night</a>
+                </div>
+                </div>
             </motion.div>
             }
                 </AnimatePresence>
