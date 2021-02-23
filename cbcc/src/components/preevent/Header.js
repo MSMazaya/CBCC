064f1912dd813db0e6c6c4355da19adebc54affc
MazayaCode <msulthanmazaya@gmail.com>
2021-02-23 10:34:11 +0700

    Add pre-event content

--- cbcc/src/components/preevent/Header.js
+++ cbcc/src/components/preevent/Header.js
@@ -31,9 +31,8 @@ const Header = () => {
                 onClick={()=>scroll(1230)}>Timeline</motion.a>
             </div>
             <div className="description">
-                <p>Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body </p>
-            </div>
-            <div className="adder-preevent"></div>
+                <p>CBCC Talks will be delivered by 3 speakers in one frame. They will share their stories and followed by tips and tricks on how to attain excellent academic and non-academic achievements to the audience. The speakers’ high influencing skills are expected to augment the promotion and attractiveness of the CBCC 2021 among Indonesian undergraduate students across the globe. While this talk show is promoting CBCC’s main event, it also gives benefits to the audience. These benefits are expected to make the audience to become more eager in participating in the main event.</p>
+            </div>>
         </motion.div>
     )
 }
