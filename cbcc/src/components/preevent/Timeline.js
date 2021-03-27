75b7750990b3f141079312732e2640bee6904988
Riandy Hasan <16520117@mahasiswa.itb.ac.id>
2021-03-27 18:29:57 +0800

    konten

--- cbcc/src/components/preevent/Timeline.js
+++ cbcc/src/components/preevent/Timeline.js
@@ -18,7 +18,19 @@ const timelineGroup = [
         nama:"Secret Speaker",
         posisi: "Coming soon",
         judul:"“How to Build Personal Branding in the Digital Era1”",
-        deskripsi:"Personal branding adalah salah satu kunci kesuksesan seseorang. Menyadari akan pentingnya personal branding baik di dunia kampus maupun kerja, kami mempersembahkan Instagram TakeOver yang akan mengupas seputar personal branding dari pembicara yang inspiratif dan berpengalaman.n"
+        deskripsi:"Personal branding adalah salah satu kunci kesuksesan seseorang. Menyadari akan pentingnya personal branding baik di dunia kampus maupun kerja, kami mempersembahkan Instagram TakeOver yang akan mengupas seputar personal branding dari pembicara yang inspiratif dan berpengalaman."
+    },
+    {
+        nama:"To Be Announced",
+        posisi: "Coming soon",
+        judul:"CBCC ClubHouse I",
+        deskripsi:"Coming soon."
+    },
+    {
+        nama:"To Be Announced",
+        posisi: "Coming soon",
+        judul:"CBCC ClubHouse II",
+        deskripsi:"Coming soon."
     }
 ]
 const Timeline = () => {
@@ -29,7 +41,7 @@ const Timeline = () => {
             <div className='flex-row pointer-wrapper'>
                 <div onClick={()=>setGroup(0)} className="pointer">
                     <div className="temlen">
-                    <a className="tanggal">5 Februari</a>
+                    <a className="tanggal" style={{left:"30px"}}>5 Februari 2021</a>
                     <a className="lipe">Live IG 1</a>
                     <a className="jdl">“Introduction of Collegacy Bandung and CBCC”</a>
                     </div>
@@ -37,16 +49,30 @@ const Timeline = () => {
                 </div>
                 <div onClick={()=>setGroup(1)} className="pointer">
                 <div className="temlen">
-                    <a className="tanggal">20 Februari</a>
+                    <a className="tanggal"style={{left:"30px"}}>20 Februari 2021</a>
                     <a className="lipe2">Live IG 2</a>
                     <a className="jdl2">“Break Your Limit ”</a>
                     </div>
                 </div>
                 <div onClick={()=>setGroup(2)} className="pointer">
                 <div className="temlen">
-                    <a className="tanggal">20 Maret</a>
-                    <a className="lipe">Live IG 3</a>
-                    <a className="jdl">“Outstanding Students 101”</a>
+                    <a className="tanggal"style={{left:"50px"}}>3 April 2021</a>
+                    <a className="lipe"style={{lineHeight:"30px",top:"50px",width:"300px",left:"-20px"}}>Instagram TakeOver</a>
+                    <a className="jdl">How to Build Personal Branding in the Digital Era</a>
+                    </div>
+                </div>
+                <div onClick={()=>setGroup(3)} className="pointer">
+                <div className="temlen">
+                    <a className="tanggal"style={{left:"45px"}}>17 April 2021</a>
+                    <a className="lipe2"style={{width:"300px",left:"35px"}}>ClubHouse I</a>
+                    <a className="jdl2"style={{top:"-70px",width:"400px",left:"-100px",fontSize:"18px",lineHeight:"15px"}}>Outstanding Students 101: How to Become the Next Outstanding Student</a>
+                    </div>
+                </div>
+                <div onClick={()=>setGroup(4)} className="pointer">
+                <div className="temlen">
+                    <a className="tanggal"style={{left:"55px"}}>2 Mei 2021</a>
+                    <a className="lipe"style={{width:"300px",left:"35px"}}>ClubHouse II</a>
+                    <a className="jdl"style={{width:"300px",left:"-30px"}}>To Be Announced</a>
                     </div>
                 </div>
             </div>
