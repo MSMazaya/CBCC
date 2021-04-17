80bdce61bebef6cd02435b62e8dfcf545bf92a08
Riandy Hasan <16520117@mahasiswa.itb.ac.id>
2021-04-18 00:15:21 +0800

    bismillah finalisasi timeline

--- cbcc/src/components/competition/Timeline.js
+++ cbcc/src/components/competition/Timeline.js
@@ -1,160 +1,80 @@
 import React, {useState} from 'react'
-import {motion} from 'framer-motion'
+import {motion, AnimatePresence} from 'framer-motion'
+
+const tl_content_pre = [
+    {date:"25-30 April 2021", title:"Open Early Bird Registration"},
+    {date:"1-15 Mei 2021", title:"Open Normal Registration"},
+    {date:"19 Mei 2021", title:"Case Distribution"},
+    {date:"22 Mei", title:"”Workshop “How to Design Your Way of Thinking”"},
+    {date:"19-31 Mei 2021", title:"Essay Submission"},
+    {date:"11 Juni 2021", title:"“Top 60 Semifinalist Announcement“"},
+]
+
+const tl_content_bisnis = [
+    {date:"12 Juni 2021", title:"Technical Meeting Collaboration Challenge"},
+    {date:"12 Juni 2021", title:"Workshop “How to Make a Proper Pitch Deck”"},
+    {date:"13-18 Juni 2021", title:"Pitchdeck Execution"},
+    {date:"18 Juni 2021", title:"Pitchdeck Submission"},
+    {date:"19 Juni 2021", title:"Pitching Day and Top 5 Anouncement"},
+    {date:"20 Juni 2021", title:"Final Presentation & Awarding Night"},
+]
 
 const Timeline = () => {
     const [timelineTypes, setTimelineTypes] = useState(true)
     return (
-        <div className="preevent-timeline flex-col">
+        <div className="timeline">
             <h1 className="sub-title">Timeline</h1>
             <div className="judul">
                 <a whileHover={()=>{}} className={timelineTypes ? "picked" : ""} onClick={()=>setTimelineTypes(true)}>Individual Preliminary</a>
                 <a className={timelineTypes ? "" : "picked"} onClick={()=>setTimelineTypes(false)}>Collaboration Challenge</a>
             </div>
-            {timelineTypes ? 
-                    <div className='flex-row pointer-wrapper'>            
-                        <div className="pointer" style={{width:"150px"}}>
-                        <div className="temlen">
-                    <a className="tanggal2" style={{
-                        left:"28px",fontSize:"15px",top:"2.5px"
-                    }}>25-30 April 2021</a>
-                    <a className="jdl3" style={{fontSize:"17px",top:"50px",left:"-70px"}}>Open Early Bird Registration</a>
-                    </div>
-                            </div>
-                        <div className="pointer"style={{width:"150px"}}>
-                        <div className="temlen">
-                    <a className="tanggal2" style={{
-                        left:"28px",fontSize:"17px",top:"2.5px"
-                    }}>1-15 Mei 2021</a>
-                    <a className="jdl4" style={{
-                        top:"-35px",
-                        left:"-70px",
-                        fontSize:"17px",
-                    }}>Open Normal
-                    Registration</a>
-                    </div>
-                            </div>
-                        <div className="pointer" style={{width:"150px"}}>
-                        <div className="temlen">
-                    <a className="tanggal2" style={{
-                        left:"34px",fontSize:"17px",top:"2.5px"
-                    }}>19 Mei 2021</a>
-                    <a className="jdl3" style={{fontSize:"17px",top:"50px",left:"-70px"}}>Case Distribution</a>
-                    </div>
-                            </div>
-                        <div className="pointer"style={{width:"150px"}}>
-                        <div className="temlen">
-                            <a className="tanggal2" style={{
-                        left:"35px",fontSize:"17px",top:"2.5px"
-                    }}>22 Mei 2021</a>
-                            <a className="jdl4" style={{
-                        top:"-38px",
-                        left:"-70px",
-                        fontSize:"17px",
-                        lineHeight:"16px"
-                    }}>Workshop “How to Design Your Way of Thinking”</a>
+            <div className="tl-gap"></div>
+            <div className='tl-wrapper'>
+                <AnimatePresence>
+                {/* <div className="line" style={{height:(timelineTypes ? "119vh" : "130vh")}}></div> */}
+                {timelineTypes ? 
+                <motion.div 
+                intial={{y:0,opacity:1}}
+                exit={{y:10,opacity:0}}
+                className="timeline-container"> 
+                {tl_content_pre.map(
+                    (x,index)=>{
+                    return <div className="tl-items">
+                            <div className="circle tl"></div>
+                            <div className="line" style={{backgroundColor:(index === tl_content_pre.length-1 && "transparent"), height:"220%"}}></div>
+                            <div className="inside-tl-items">
+                                <h3 className="tl-date">{x.date}</h3>
+                                <h3>{x.place}</h3>
+                                <span>{x.title}</span>
                             </div>
                         </div>
-                        <div className="pointer"style={{width:"150px"}}>
-                        <div className="temlen">
-                            <a className="tanggal2" style={{
-                        left:"26px",fontSize:"17px",top:"2.5px"
-                    }}>19-31 Mei 2021</a>
-                            <a className="jdl3" style={{
-                        top:"60px",
-                        left:"-70px",
-                        fontSize:"17px",
-                        lineHeight:"16px"
-                    }}>Essay Submission</a>
-                        </div>
-                            </div>
-                            <div className="pointer"style={{width:"150px"}}>
-                        <div className="temlen">
-                            <a className="tanggal2" style={{
-                        left:"32px",fontSize:"17px",top:"2.5px"
-                    }}>11 Juni 2021</a>
-                            <a className="jdl3" style={{
-                        top:"-30px",
-                        left:"-70px",
-                        fontSize:"17px",
-                        lineHeight:"16px"
-                    }}>Top 60 Semifinalist Announcement</a>
-                        </div>
-                            </div>
-
-                    </div>
-
-                    
-                    
+                    }
+                )
+                }
+            </motion.div>
             :
-            <div className='flex-row pointer-wrapper'>           
-                      <div className="pointer" style={{width:"150px"}}>
-                        <div className="temlen">
-                    <a className="tanggal2" style={{
-                        left:"34px",fontSize:"18px",top:"2.5px"
-                    }}>12 Juni 2021</a>
-                    <a className="jdl3" style={{fontSize:"17px",top:"56px",left:"-30px",width:"200px",lineHeight:"15px"}}>Workshop “How to Make a Proper Pitch Deck”</a>
-                    <a className="jdl4" style={{
-                        top:"-45px",
-                        lineHeight:"15px",
-                        left:"-30px",
-                        fontSize:"17px", width:"200px"
-                    }}>Technical Meeting
-                    Collaboration
-                    Challenge</a>
-                    </div>
+            <motion.div
+            exit={{y:10,opacity:0}}
+            className="timeline-container">
+                {tl_content_bisnis.map(
+                    (x,index)=>{
+                    return (
+                        <div className="tl-items">
+                            <div className="circle tl"></div>
+                            <div className="line" style={{backgroundColor:(index === tl_content_bisnis.length-1 && "transparent"), height:"230%"}}></div>
+                            <div className="inside-tl-items">
+                                <h4>{x.title}</h4>
+                                <span>{x.date}</span>
                             </div>
-                        <div className="pointer" style={{width:"150px"}}>
-                        <div className="temlen">
-                    <a className="tanggal2" style={{
-                        left:"28px",fontSize:"15px",top:"2.5px"
-                    }}>13-18 Juni 2021</a>
-                    <a className="jdl3" style={{fontSize:"17px",top:"50px",left:"-70px"}}>Pitchdeck
-Execution</a>
-                    </div>
-                            </div>
-                        <div className="pointer"style={{width:"150px"}}>
-                        <div className="temlen">
-                            <a className="tanggal2" style={{
-                        left:"35px",fontSize:"17px",top:"2.5px"
-                    }}>18 Juni 2021</a>
-                            <a className="jdl4" style={{
-                        top:"-30px",
-                        left:"-70px",
-                        fontSize:"17px",
-                        lineHeight:"16px"
-                    }}>Pitchdeck 
-                    Submission</a>
-                            </div>
-                        </div>
-                        <div className="pointer"style={{width:"150px"}}>
-                        <div className="temlen">
-                            <a className="tanggal2" style={{
-                        left:"26px",fontSize:"17px",top:"2.5px"
-                    }}>19 Juni 2021</a>
-                            <a className="jdl3" style={{
-                        ontSize:"17px",top:"55px",left:"-20px",width:"200px",lineHeight:"15px",fontSize:"17px"
-                    }}>Pitching Day
-                    and 
-                    Top 5 Anouncement</a>
-                        </div>
-                            </div>
-                            <div className="pointer"style={{width:"150px"}}>
-                        <div className="temlen">
-                            <a className="tanggal2" style={{
-                        left:"32px",fontSize:"17px",top:"2.5px"
-                    }}>20 Juni 2021</a>
-                            <a className="jdl3" style={{
-                       top:"-45px",
-                       lineHeight:"15px",
-                       left:"-30px",
-                       fontSize:"17px", width:"200px"
-                    }}>Final Presentation
-                    & Awarding Night</a>
-                        </div>
-                            </div>
-            </div>
+                        </div> 
+                        )
+                    }
+                )
+                }
+            </motion.div>
             }
-            
+                </AnimatePresence>   
+            </div>
             <div className="flex-row"
                 style={{
                     gap:"30px",
@@ -190,6 +110,7 @@ Execution</a>
                 </div></motion.div>
             </div>
         </div>
+        
     )
 }
 
